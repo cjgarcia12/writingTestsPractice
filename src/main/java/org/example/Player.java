b46e34b063e284f286d93ff31f64d8b1307c24b7
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-08-14 16:52:11 -0400

    init commit

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,21 @@
+package org.example;
+
+//implementation of blackjack player
+public class Player {
+    private int money;
+
+    public Player() {
+        this.money = 1000;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+
+
+}
